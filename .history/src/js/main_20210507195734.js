738e898f57b9ed10012842efceff3fe138183b01
Алихан Гаджиев <alihangadziev@MacBook-Air-Alihan.local>
2021-05-07 21:05:32 +0600

    animation

---
+++ .history/src/js/main_20210507195734.js
@@ -0,0 +1,17 @@
+var angle = 0;
+
+function galleryspin(sign) {
+    spinner = document.querySelector("#spinner");
+    if (!sign) {
+        angle = angle + 45;
+    } else {
+        angle = angle - 45;
+    }
+    spinner.setAttribute("style", "-webkit-transform: rotateY(" + angle + "deg); -moz-transform: rotateY(" + angle + "deg); transform: rotateY(" + angle + "deg);");
+}
+
+$(window).scroll(function(){
+    if ( $(this).scrollTop() > $('.wrapper__svg-circle').offset().top - 300 ) {
+        $('.wrapper__svg-circle').addClass('wrapper__svg-circle_active');
+    }
+});
